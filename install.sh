30ec71eb73c5c93cf352afca0c89190d1955d837
Nguyễn Khoa <42170045+neko941@users.noreply.github.com>
2022-07-31 12:19:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,27 @@ sudo pacman -Syy
 sudo pacman -S --needed base-devel
 sudo pacman -S git 
 
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd
-git clone https://aur.archlinux.org/snapd.git && cd snapd && makepkg -si && sudo systemctl enable --now snapd.socket && sudo ln -s /var/lib/snapd/snap /snap && cd
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+cd
+
+# VNI
+yay -S pamac-aur-git
+echo "" >> ~/.bashrc
+echo "export GTK_IM_MODULE=ibus" >> ~/.bashrc
+echo "export GTK_IM_MODULE=ibus" >> ~/.bashrc
+echo "export QT_IM_MODULE=ibus" >> ~/.bashrc
+echo "export XMODIFIERS=@im=ibus" >> ~/.bashrc
+echo "export QT4_IM_MODULE=ibus" >> ~/.bashrc
+echo "export CLUTTER_IM_MODULE=ibus" >> ~/.bashrc
+echo "ibus-daemon -drx" >> ~/.bashrc
 
 sudo pacman -S firefox
 sudo pacman -S vlc
